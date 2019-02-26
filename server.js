c7bfca7712a078d896a138a8a891ee3bbfee01a9
Cory Henderson <chenderson19899@gmail.com>
2019-02-26 13:18:13 -0800

    completes card 3

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const Client = require('pg');
+const superagent = require('superagent');
 const app = express();
 
 // const cors = require('cors');
@@ -12,9 +13,23 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 
+//ROUTES
 app.get('/hello', proofOfLife);
 app.get('/', loadSearch);
+app.post('/searches', createSearch);
 
+//CONSTRUCTOR FUNCTIONS
+function Book(book){
+  this.title = book.title || 'Title does not exist';
+  this.author = book.authors || 'Author does not exist';
+  this.ISBN = book.ISBN || 'ISBN does not exist';
+  this.image_url = book.imageLinks.smallThumbnail || 'Image does not exist';
+  this.description = book.description || 'Description does not exist';
+  //this.bookshelf_name = book.body.volumeInfo.bookshelf_name || 'Shelf name does not exist';
+}
+
+
+//HANDLER FUNCTIONS
 function proofOfLife (request, response) {
   response.render('pages/index');
   // app.use(express.static('./public'));
@@ -24,5 +39,19 @@ function loadSearch(request, response) {
   response.render('pages/index');
 }
 
+function createSearch(request, response) {
+  let url = 'https://www.googleapis.com/books/v1/volumes?q=';
+  console.log('request.body', request.body);
+
+  if(request.body.search[1] === 'title') {url += `+intitle:${request.body.search[0]}`;}
+  if(request.body.search[1] === 'author') {url += `+inauthor:${request.body.search[0]}`;}
+  console.log(url);
+
+  superagent.get(url)
+    .then(apiResponse => apiResponse.body.items.map(bookResult => new Book(bookResult.volumeInfo)))
+    .then(results => response.render('pages/searches/show', { searchesResults: results}));
+}
+
+//new Book(bookResult.volumeInfo)
 // app.use('*', (request,response) => res.render('error'));
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
---
+++ tester.js
@@ -0,0 +1,48 @@
+{ kind: 'books#volume',
+  id: 'NambWzFD-mkC',
+  etag: 'jbiUg/roNLc',
+  selfLink: 'https://www.googleapis.com/books/v1/volumes/NambWzFD-mkC',
+  volumeInfo:
+   { title: 'Jungle Book',
+     authors: [ 'Walt Disney Productions', 'Walt Productions Sta Disney' ],
+     publisher: 'Mouse Works',
+     publishedDate: '1992-08',
+     description:
+      'The animal friends of the boy raised by wolves escort himout of the jungle to keep him safe from the tiger who seeks to kill him.',
+     industryIdentifiers: [ [Object], [Object] ],
+     readingModes: { text: false, image: false },
+     pageCount: 94,
+     printType: 'BOOK',
+     categories: [ 'Music' ],
+     maturityRating: 'NOT_MATURE',
+     allowAnonLogging: false,
+     contentVersion: '0.0.1.0.preview.0',
+     imageLinks:
+      { smallThumbnail:
+         'http://books.google.com/books/content?id=NambWzFD-mkC&printsec=frontcover&img=1&zoom=5&source=gbs_api',
+        thumbnail:
+         'http://books.google.com/books/content?id=NambWzFD-mkC&printsec=frontcover&img=1&zoom=1&source=gbs_api' },
+     language: 'en',
+     previewLink:
+      'http://books.google.com/books?id=NambWzFD-mkC&q=intitle:Jungle+Book&dq=intitle:Jungle+Book&hl=&cd=10&source=gbs_api',
+     infoLink:
+      'http://books.google.com/books?id=NambWzFD-mkC&dq=intitle:Jungle+Book&hl=&source=gbs_api',
+     canonicalVolumeLink:
+      'https://books.google.com/books/about/Jungle_Book.html?hl=&id=NambWzFD-mkC' },
+  saleInfo:
+   { country: 'US', saleability: 'NOT_FOR_SALE', isEbook: false },
+  accessInfo:
+   { country: 'US',
+     viewability: 'NO_PAGES',
+     embeddable: false,
+     publicDomain: false,
+     textToSpeechPermission: 'ALLOWED',
+     epub: { isAvailable: false },
+     pdf: { isAvailable: false },
+     webReaderLink:
+      'http://play.google.com/books/reader?id=NambWzFD-mkC&hl=&printsec=frontcover&source=gbs_api',
+     accessViewStatus: 'NONE',
+     quoteSharingAllowed: false },
+  searchInfo:
+   { textSnippet:
+      'The animal friends of the boy raised by wolves escort himout of the jungle to keep him safe from the tiger who seeks to kill him.' } }
