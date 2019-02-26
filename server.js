0ab305a13f07c20775325aa0b0b4aa97e2133722
Peter Murphy <naughtnowwhen@gmail.com>
2019-02-26 11:52:40 -0800

    proof of life working except for css

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 const express = require('express');
 const Client = require('pg');
 const app = express();
-app.use(cors());
+
+// const cors = require('cors');
+// app.use(cors());
+
+app.use(express.static('./public'));
+app.set('view engine', 'ejs');
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/hello', proofOfLife);
+
+function proofOfLife (request, response) {
+    response.render('pages/index');
+    // app.use(express.static('./public'));
+}
+
+// app.use('*', (request,response) => res.render('error'));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
